4049ad2eb1ba9928ae26738c3aa764ebc63d078e
PiotrB <piotrb@gmail.com>
2013-06-04 13:32:11 +0200

    Unused usings

--- ForexTester/ForexTester/Tester.cs
+++ ForexTester/ForexTester/Tester.cs
@@ -4,9 +4,7 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
 using ForexTester.Strategies;
 
 namespace ForexTester
