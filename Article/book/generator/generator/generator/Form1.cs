7814b71c6e50dda1eeac2c8f2fe3c536ce8004dd
PiotrB <piotrb@gmail.com>
2013-10-29 02:41:41 +0100

    book & book generator

---
+++ Article/book/generator/generator/generator/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using generator.Properties;
+
+namespace generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerateClick(object sender, System.EventArgs e)
+        {
+            var outputFolder = "output" + DateTime.Now.ToString("yyyyMMdd_hhmmss");
+            System.IO.Directory.CreateDirectory(outputFolder);
+            System.IO.Directory.CreateDirectory(Path.Combine(outputFolder, "images"));
+            var oneChartGeneratorMz = new ChartGenerator(outputFolder, "MZ");
+            var s = new System.Text.StringBuilder(); 
+            s.Append(oneChartGeneratorMz.Generate("cadchfS1{0}", "CADCHF"));
+            s.Append(oneChartGeneratorMz.Generate("fcopperS1{0}", "FCOPPER"));
+            s.Append(oneChartGeneratorMz.Generate("fcornS1{0}", "FCORN"));
+            s.Append(oneChartGeneratorMz.Generate("nzdjpyS1{0}", "NZDJPY"));
+            s.Append(oneChartGeneratorMz.Generate("usdcadS1{0}", "USDCAD"));
+            File.WriteAllText(Path.Combine(outputFolder, "MZ.tex"), s.ToString());
+            toolStripStatusLabel1.Text = Resources.btnGenerateClickGenerated;
+        }
+    }
+}
