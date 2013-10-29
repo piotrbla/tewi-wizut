eae0cf57509baf1e3efce853311711c7b06e368c
PiotrB <piotrb@gmail.com>
2013-10-29 02:53:51 +0100

    book format proposal - work in progress ;)

--- Article/book/generator/generator/generator/Form1.cs
+++ Article/book/generator/generator/generator/Form1.cs
@@ -18,13 +18,21 @@ namespace generator
             System.IO.Directory.CreateDirectory(outputFolder);
             System.IO.Directory.CreateDirectory(Path.Combine(outputFolder, "images"));
             var oneChartGeneratorMz = new ChartGenerator(outputFolder, "MZ");
-            var s = new System.Text.StringBuilder(); 
-            s.Append(oneChartGeneratorMz.Generate("cadchfS1{0}", "CADCHF"));
-            s.Append(oneChartGeneratorMz.Generate("fcopperS1{0}", "FCOPPER"));
-            s.Append(oneChartGeneratorMz.Generate("fcornS1{0}", "FCORN"));
-            s.Append(oneChartGeneratorMz.Generate("nzdjpyS1{0}", "NZDJPY"));
-            s.Append(oneChartGeneratorMz.Generate("usdcadS1{0}", "USDCAD"));
-            File.WriteAllText(Path.Combine(outputFolder, "MZ.tex"), s.ToString());
+            var builderMz = new System.Text.StringBuilder();
+            builderMz.Append(oneChartGeneratorMz.Generate("cadchfS1{0}", "CADCHF"));
+            builderMz.Append(oneChartGeneratorMz.Generate("fcopperS1{0}", "FCOPPER"));
+            builderMz.Append(oneChartGeneratorMz.Generate("fcornS1{0}", "FCORN"));
+            builderMz.Append(oneChartGeneratorMz.Generate("nzdjpyS1{0}", "NZDJPY"));
+            builderMz.Append(oneChartGeneratorMz.Generate("usdcadS1{0}", "USDCAD"));
+            File.WriteAllText(Path.Combine(outputFolder, "MZ.tex"), builderMz.ToString());
+            var oneChartGeneratorAb = new ChartGenerator(outputFolder, "AB");
+            var builderAb = new System.Text.StringBuilder();
+            builderAb.Append(oneChartGeneratorAb.Generate("chfjpy{0}", "CHFJPY"));
+            builderAb.Append(oneChartGeneratorAb.Generate("eurjpy{0}", "EURJPY"));
+            builderAb.Append(oneChartGeneratorAb.Generate("fus500{0}", "FUS500"));
+            builderAb.Append(oneChartGeneratorAb.Generate("gbpchf{0}", "GBPCHF"));
+            builderAb.Append(oneChartGeneratorAb.Generate("silver{0}", "SILVER"));
+            File.WriteAllText(Path.Combine(outputFolder, "AB.tex"), builderAb.ToString());
             toolStripStatusLabel1.Text = Resources.btnGenerateClickGenerated;
         }
     }
